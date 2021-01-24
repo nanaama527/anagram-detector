1783943be2c1717dc7699c1219dcf66d84927e4b
Nana <72888369+nanaama527@users.noreply.github.com>
2021-01-24 17:08:56 -0500

    commit tree c194477e64a10c922a2febecc8bc0b8053ee1f1b

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram
+    def initialize(word)
+        @word = word
+    end
+    
+    def match(array)
+        array.select do |word|
+            word.split("").sort == @word.split("").sort
+        end
+    end
+end
+
